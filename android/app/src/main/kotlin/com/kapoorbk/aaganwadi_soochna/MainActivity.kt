b8bbc3a465826af6972005ca606c012291b22ed3
Anshul Kapoor <anshulkapoor306@gmail.com>
2024-08-01 18:14:05 +0530

    flutter boilerplate added

---
+++ android/app/src/main/kotlin/com/kapoorbk/aaganwadi_soochna/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.kapoorbk.aaganwadi_soochna
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/View/splash_screen.dart
@@ -0,0 +1,72 @@
+import 'dart:async';
+import 'package:aaganwadi_soochna/View/getStartedPG.dart';
+import 'package:flutter/material.dart';
+import 'dart:math' as math;
+
+class SplashScreen extends StatefulWidget {
+  const SplashScreen({super.key});
+
+  @override
+  State<SplashScreen> createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen>
+    with TickerProviderStateMixin {
+  late final AnimationController controller = AnimationController(
+    duration: const Duration(seconds: 3),
+    vsync: this,
+  )..repeat();
+
+  @override
+  void initState() {
+   
+    super.initState();
+    Timer(const Duration(seconds: 6), () {
+      Navigator.push(
+        context,
+        MaterialPageRoute(builder: (context) => const GetStarted()),
+      );
+    });
+  }
+  
+  @override
+  void dispose() {
+    controller.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              AnimatedBuilder(
+                  animation: controller,
+                  child: Container(
+                    height: 300,
+                    width: 300,
+                    child: const Center(
+                      child: Image(image: AssetImage('images/agwdr.png')),
+                    ),
+                  ),
+                  builder: (BuildContext context, Widget? child) {
+                    return Transform.rotate(
+                      angle: controller.value * 2.0 * math.pi,
+                      child: child,
+                    );
+                  }),
+              SizedBox(
+                height: MediaQuery.of(context).size.height * 0.08,
+              ),
+              
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
